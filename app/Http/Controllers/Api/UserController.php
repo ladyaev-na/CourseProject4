d362829ad29bcbff912bba75919559f54fd6ea6c
Fatalex2005 <ruginleha@gmail.com>
2024-12-03 13:13:50 +0700

    Маленькие изменения UserController.php(index)

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -19,8 +19,8 @@ class UserController extends Controller
             return response()->json(['message' => 'У вас нет прав на выполнение этого действия'], 403);
         }
 
-        $users = User::all();
-        return response()->json($users)->setStatusCode(200,'Ок');
+        $couriers = User::where('role_id', 2)->get(); // Предполагаем, что role_id=2 это курьер
+        return response()->json($couriers)->setStatusCode(200, 'Ок');
     }
     public function show($id)
     {
