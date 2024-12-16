ca2ae4f1fb6b7829c7bb98f5ff80bf4a06b74c55
Fatalex2005 <ruginleha@gmail.com>
2024-12-16 19:29:17 +0700

    AccessSeeder.php дополнил.

--- database/seeders/AccessSeeder.php
+++ database/seeders/AccessSeeder.php
@@ -18,7 +18,42 @@ class AccessSeeder extends Seeder
             'startChange' => '08:00:00',
             'endChange' => '23:00:00',
             'confirm' => false,
-            'user_id' => 1
+            'user_id' => 2
+        ]);
+        Access::create([
+            'date' => '2023-10-02',
+            'startChange' => '08:00:00',
+            'endChange' => '17:00:00',
+            'confirm' => false,
+            'user_id' => 2
+        ]);
+        Access::create([
+            'date' => '2023-10-02',
+            'startChange' => '18:00:00',
+            'endChange' => '23:00:00',
+            'confirm' => false,
+            'user_id' => 2
+        ]);
+        Access::create([
+            'date' => '2023-10-03',
+            'startChange' => '08:00:00',
+            'endChange' => '13:00:00',
+            'confirm' => false,
+            'user_id' => 2
+        ]);
+        Access::create([
+            'date' => '2023-10-03',
+            'startChange' => '14:00:00',
+            'endChange' => '18:00:00',
+            'confirm' => false,
+            'user_id' => 2
+        ]);
+        Access::create([
+            'date' => '2023-10-03',
+            'startChange' => '19:00:00',
+            'endChange' => '23:00:00',
+            'confirm' => false,
+            'user_id' => 2
         ]);
     }
 }
