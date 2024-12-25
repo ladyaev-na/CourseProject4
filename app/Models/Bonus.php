95cc335dd24b85c509be6e98fa4be4e06d8161f0
Fatalex2005 <ruginleha@gmail.com>
2024-12-25 13:51:16 +0700

    Полностью готовая связь бонусов с ролями.

--- app/Models/Bonus.php
+++ app/Models/Bonus.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bonus extends Model
 {
-    protected $fillable = ['title', 'description','price'];
+    protected $fillable = ['title', 'description','price', 'role_id'];
     public function role()
     {
         return $this->belongsTo(Role::class);
